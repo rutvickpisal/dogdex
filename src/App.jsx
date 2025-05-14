20e8928d11b43ea348d85698bb97399e5f90780a
rutvickpisal <rutvick.pisal@gmail.com>
2025-05-14 20:24:22 +0530

    added basic auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./Pages/Home";
+import PrivateRoute from "./Components/PrivateRoute";
+import Login from "./Pages/Login";
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
   );
 }
 
