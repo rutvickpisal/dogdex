94fc26934c71c1ddacb6ded6f4ff6ff101bf68fb
Rutvick <rutvick.pisal@gmail.com>
2025-05-15 16:16:55 +0530

    updated breed details page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import PrivateRoute from "./Components/PrivateRoute";
 import Login from "./Pages/Login";
 import Navbar from "./Components/Navbar";
 import { useAuth } from "./contexts/AuthContext";
+import BreedsList from "./Pages/BreedList";
+import BreedDetails from "./Pages/BreedDetails";
 
 function App() {
   const {user} = useAuth();
@@ -21,6 +23,10 @@ function App() {
           </PrivateRoute>
         }
       />
+      <Route path="/breeds" element={<PrivateRoute><BreedsList /></PrivateRoute>} />
+      <Route path="/breeds/:breedName" element={<PrivateRoute><BreedDetails /></PrivateRoute>} />
+      <Route path="/breeds/:breedName/:origin" element={<PrivateRoute><BreedDetails /></PrivateRoute>} />
+
     </Routes>
     </>
   );
